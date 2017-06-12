d53343bd38d0b06f8e610b1ad9e123abbc9c2381
Sankruth Kota <sankruth.kota@gmail.com>
2017-06-12 18:40:46 -0400

    ok i needa fix this will do later
    [ just tried redoing it]

--- templatematch.py
+++ templatematch.py
@@ -1,15 +1,16 @@
 import cv2
-import numpy as np
+import numpy as numpy
 
-img_rgb = cv2.imread('test.jpg')
-img_grey = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+img_bgr = cv2.imread('test.png')
+img_gray = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2GRAY)
 
-template = cv2.imread('waldo.jpg', 0)
+template = cv2.imread('waldo.jpg',0)
 w, h = template.shape[::-1]
-res = cv2.matchTemplate(img_grey, template, cv2.TM_CCOEFF_NORMED)
-threshold = 0.8
-loc = np.where( res>= threshold)
-for pt in zip(*loc[::-1]):
-    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,255,255), 2)
 
-cv2.imshow('WaldoFinder',img_rgb)
+res = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
+threshold = 0.8
+loc = np.where(res>= threshold)
+
+for pt in zip(*loc[::-1]):
+	cv2.rectangle(img_bgr, pt, (pt[0]+w, pt[1]+h), (0,255,255), 2)
+cv2.imshow('detected',img_bgr) 
