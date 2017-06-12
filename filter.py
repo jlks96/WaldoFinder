e439fbd0b4e5e5be249177dcb44c592c344cfa88
Sankruth Kota <sankruth.kota@gmail.com>
2017-06-11 22:06:41 -0400

    uploaded a better image

--- filter.py
+++ filter.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
  
-img = cv2.imread('test.png', 1)
+img = cv2.imread('test.jpg', 1)
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 lower_range = np.array([169, 100, 100], dtype=np.uint8)
 upper_range = np.array([189, 255, 255], dtype=np.uint8)
