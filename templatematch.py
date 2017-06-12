68fe084e530354499e8aef6a5758cdafa6b006b7
Sankruth Kota <sankruth.kota@gmail.com>
2017-06-12 18:25:14 -0400

    updated template matching + template

--- templatematch.py
+++ templatematch.py
@@ -5,3 +5,11 @@ img_rgb = cv2.imread('test.jpg')
 img_grey = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
 
 template = cv2.imread('waldo.jpg', 0)
+w, h = template.shape[::-1]
+res = cv2.matchTemplate(img_grey, template, cv2.TM_CCOEFF_NORMED)
+threshold = 0.8
+loc = np.where( res>= threshold)
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,255,255), 2)
+
+cv2.imshow('WaldoFinder',img_rgb)
