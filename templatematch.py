ee4915a48f31e7c4250e4ac392c1cfe8437e7559
Sankruth Kota <sankruth.kota@gmail.com>
2017-06-13 12:34:59 -0400

    big changes

--- templatematch.py
+++ templatematch.py
@@ -1,16 +1,26 @@
 import cv2
-import numpy as numpy
+import numpy as np
+from matplotlib import pyplot as plt
 
-img_bgr = cv2.imread('test.png')
-img_gray = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2GRAY)
 
+img = cv2.imread('test.jpg',0)
+img2 = img.copy()
 template = cv2.imread('waldo.jpg',0)
 w, h = template.shape[::-1]
-
-res = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
-threshold = 0.8
-loc = np.where(res>= threshold)
-
-for pt in zip(*loc[::-1]):
-	cv2.rectangle(img_bgr, pt, (pt[0]+w, pt[1]+h), (0,255,255), 2)
-cv2.imshow('detected',img_bgr) 
+# All the 6 methods for comparison in a list
+methods = ['cv2.TM_CCOEFF_NORMED']
+for meth in methods:
+    img = img2.copy()
+    method = eval(meth)
+    # Apply template Matching
+    res = cv2.matchTemplate(img,template,method)
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+    top_left = max_loc
+    bottom_right = (top_left[0] + w, top_left[1] + h)
+    cv2.rectangle(img,top_left, bottom_right, 255, 2)
+    plt.subplot(121),plt.imshow(res,cmap = 'gray')
+    plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
+    plt.subplot(122),plt.imshow(img,cmap = 'gray')
+    plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
+    plt.suptitle(meth)
+    plt.show()
