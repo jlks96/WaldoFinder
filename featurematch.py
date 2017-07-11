fa68030fd8742b41c8c465a83f2f6286c7e87c18
Sankruth Kota <sankruth.kota@gmail.com>
2017-07-11 14:22:00 -0400

    updated with how exactly it's going to work

--- featurematch.py
+++ featurematch.py
@@ -8,3 +8,16 @@ import matplotlib.pyplot as plt
 
 template = cv2.imread('waldo.jpg', 0)
 search = cv2.imread('test.jpg', 0)
+
+# Our detector
+orb = cv2.ORB()
+
+kp1, des1 = orb.detectAndCompute(template, None)
+kp2, des2 = orb.detectAndCompute(search, None)
+
+# Hamming is used to determine distance
+
+bruteforce = cv2.BFMatcher(cv2.NORM_HAMMING, crosscheck = True)
+
+
+
