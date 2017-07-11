3ff9652d8a517cd509eb645cef82fe335a6d01b7
Sankruth Kota <sankruth.kota@gmail.com>
2017-07-11 14:15:33 -0400

    added some annotations to improve readibility

--- featurematch.py
+++ featurematch.py
@@ -2,5 +2,9 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
+# Loaded the images
+# Template is the picture that we are trying to find
+# Search is the image we are looking in
+
 template = cv2.imread('waldo.jpg', 0)
 search = cv2.imread('test.jpg', 0)
