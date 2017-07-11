2ba70224f507bec7081e40ae5838275c322de461
Sankruth Kota <sankruth.kota@gmail.com>
2017-07-11 15:09:03 -0400

    need to work on loading the images properly

--- featurematch.py
+++ featurematch.py
@@ -3,17 +3,6 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
-# Loaded the images
-# Template is the picture that we are trying to find
-# Search is the image we are looking in
-
-template = cv2.imread('waldo.jpg', 0)
-search = cv2.imread('test.jpg', 0)
-test = cv2.cvtColor(template, cv2.COLOR_BGR2HSV)
-plt.imshow(test), plt.show()
-
-
-plt.imshow(search), plt.show()
 
 # Our detector
 orb = cv2.ORB()
